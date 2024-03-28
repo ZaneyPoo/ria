13dbd47712e74e9d09efc66f561c8df4e4a13147
Zane <zanehargis@gmail.com>
2024-03-28 03:28:02 -0700

    add file indicators, limited color support, and file hiding

--- rls/README.md
+++ rls/README.md
@@ -1,11 +1,27 @@
 #### TODO 
 
 ##### Core
-- [] List names of files in current dir
-- [] List names of files in specific dir
-- [] Accept multiple arguments and list each with labels
+- [x] List names of files in current dir
+- [x] List names of files in specific dir
+- [x] Accept multiple arguments and list each with labels
+- [] Sort names alphabetically by default
 
 ##### Options
 - [] -h, --help List a help page
 - [] -l List all files with their mode, owner, group, size, modification time
-- [] -a Include hidden files (Files starting with ".")
+- [] -F Show file indicators (/ for directories, * for executables, etc.)
+- [x] -a Include hidden files (Files starting with ".")
+
+##### Other
+- [] Load colors from LS_COLORS environment variable, falling back to default if not set
+
+###### Default colors 
+
+[Source](https://itsfoss.com/ls-color-output/)
+| Filetype | Color | ANSI Escape Sequence |
+| ---- | --- | --- |
+| Normal | White | |
+| Directory| Bold blue | |
+| Symlink | Bold cyan | |
+| Broken symlink | Red w/ black background | |
+| Executable | Bold red | |
--- rls/src/main.rs
+++ rls/src/main.rs
@@ -1,8 +1,27 @@
 use std::io::ErrorKind;
 use std::path::PathBuf;
-use std::fs;
-use std::str;
-use clap::Parser;
+use std::fs::{self, DirEntry, FileType};
+use clap::{Parser, ValueEnum};
+
+mod colors {
+    pub const WHITE: &str = "\x1b[37m"; // regular files
+    pub const BLUE: &str = "\x1b[34m"; // directories
+    pub const CYAN: &str = "\x1b[36m";
+    pub const PURPLE: &str = "\x1b[35m"; // symlinks
+    pub const _RED: &str = "\x1b[31m"; // files with executable bit set
+    pub const _GREEN: &str = "\x1b[32m"; 
+    pub const RESET: &str = "\x1b[39m";
+}
+
+mod markers {
+    pub const DIR: char = '/';
+    pub const EXE: char = '*';
+    pub const SYMLINK: char = '@';
+    pub const FIFO: char = '|';
+    pub const SOCKET: char = '=';
+    pub const DOOR: char = '>';
+}
+
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
@@ -15,6 +34,10 @@ struct Cli {
     /// Print [DIRECTORIES] as a long listing
     long_list: bool,
 
+    #[arg(long, value_enum, value_name = "WHEN")]
+    /// Color files depending on their type
+    color: Option<When>,
+
     #[arg(short, long)]
     /// Recursively list any subdirectories
     recursive: bool,
@@ -24,20 +47,71 @@ struct Cli {
 }
 
 
-fn list_directory(dir: &PathBuf, recur: bool, all: bool, long: bool) {
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
+enum When {
+    Always, 
+    Auto,
+    Never,
+}
+
+
+fn format_file(entry: &DirEntry, opts: &Cli) -> String {
+    let filetype = entry.file_type().unwrap();
+    let (color, classifier) = match filetype {
+        _ if filetype.is_dir() => (colors::BLUE, Some(markers::DIR)),
+        _ if filetype.is_symlink() => (colors::CYAN, Some(markers::SYMLINK)),
+        _ => (colors::WHITE, None),
+    };
+    format!("{color}{name}{reset}{classifier}", 
+        name = entry.file_name().into_string().unwrap(), 
+        reset = colors::RESET,
+        classifier = classifier.unwrap_or_default())
+}
+
+
+fn format_line(entries: Vec<DirEntry>, opts: &Cli) -> String {
+    let mut line = String::new();
+    for entry in entries {
+        let filename = entry.file_name().into_string().unwrap();
+        if !opts.all && filename.starts_with(".") { continue }
+
+        let formatted = format_file(&entry, opts);
+
+        line.push_str(&formatted);
+        line.push_str("  ");
+    }
+
+    line
+}
+
+
+#[allow(dead_code, unused_variables)]
+fn format_list(entries: Vec<DirEntry>, opts: &Cli) -> String {
+    todo!("Not implemented yet :(");
+}
+
+
+#[allow(unused)]
+fn list_directory(dir: &PathBuf, opts: &Cli) {
     // Needs to handle three cases:
     // 1. The directory exists and is accessible, display each item
     // 2. The directory exists but we don't have permission, display warning and continue
     // 3. The directory doesn't exist, display warning and continue
 
     let contents = fs::read_dir(dir);
+    let alpha = true;
 
     match contents {
         Ok(entries) => {
-            // TODO: Finish this or something idk
-            for entry in entries {
-                
-            }
+            let mut contents: Vec<DirEntry> = entries
+                .filter_map(|e| Some(e.unwrap()))
+                .collect();
+            let line = if opts.long_list { 
+                format_list(contents, opts) 
+            } else { 
+                format_line(contents, opts) 
+            };
+            println!("{line}");
         },
         Err(error) => { 
             let msg = match error.kind() {
@@ -56,12 +130,12 @@ fn main() {
     println!("{cli:?}");
 
     if let Some(ref dirs) = cli.directories {
+        let use_labels = dirs.len() > 1;
         for dir in dirs {
+            if use_labels { println!("{}:", dir.display()) }
             list_directory(
                 dir, 
-                cli.recursive,
-                cli.all,
-                cli.long_list,
+                &cli
             );
         }
     };
